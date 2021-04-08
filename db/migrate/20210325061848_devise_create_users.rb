79041375c37924b9633e13a4036d50509ea76105
n-masuda <m7241@me.com>
2021-04-09 00:43:05 +0900

    ユーザー登録機能の実装

--- db/migrate/20210325061848_devise_create_users.rb
+++ db/migrate/20210325061848_devise_create_users.rb
@@ -6,6 +6,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,           null: false
+      t.integer :prefecture_id,     null: false
+      t.integer :area,              null: false
+      t.integer :age,               null: false
+      t.string :part,               null: false
+      t.string :my_hero,            null: false
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,12 @@ ActiveRecord::Schema.define(version: 2021_03_25_061848) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nickname", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "area", null: false
+    t.integer "age", null: false
+    t.string "part", null: false
+    t.string "my_hero", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
