60dae2fc5cea192aca9aa6b39e39e51c0296249d
n-masuda <m7241@me.com>
2021-04-27 01:28:20 +0900

    active hash導入

--- db/migrate/20210412061928_create_wants.rb
+++ db/migrate/20210412061928_create_wants.rb
@@ -3,6 +3,7 @@ class CreateWants < ActiveRecord::Migration[6.0]
     create_table :wants do |t|
       t.string :title, null: false
       t.string :text, null: false
+      t.integer :prefecture_id,     null: false
       t.references :user, foreign_key: true
       t.timestamps
     end
